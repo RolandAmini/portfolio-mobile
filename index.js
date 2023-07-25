fc27605195846d3fc2e6f207e43dbfdd2b3560fa
Roland Amini <aminiroland01@gmail.com>
2023-07-25 15:33:24 -0700

    update linters

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
- const navicon=document.querySelector('#nav-icon')
- const navmenu=document.querySelector('.nav-menu')
- navicon.addEventListener('onclik',function(){
-    navmenu.style.display = "block";
-    navicon.style.display = "none";
- })
+const navicon = document.querySelector("#nav-icon");
+const navmenu = document.querySelector(".nav-menu");
 
-//  function ditBonjour(){
-//     alert('bonjour');
-//  }
-//  ditBonjour();
-
-//  const navIcon = document.getElementsByClassName('nav-icon')[0];
-//  const navMenu = document.getElementsByClassName('nav-menu')[0];
- 
-//  navIcon.addEventListener('click', () => {
-//      navMenu.classList.toggle('active');
-//  });
- 
+navicon.addEventListener("click", (e) => {
+  e.preventDefault();
+  navmenu.classList.toggle("show-menu");
+});
