f6d75d087327fdc2663ecb553f5e7c093f912a0f
Roland Amini <aminiroland01@gmail.com>
2023-08-05 14:43:05 -0700

    last commit

--- index.js
+++ index.js
@@ -10,6 +10,106 @@ hamburgerBtn.addEventListener("click", function (e) {
   closeBtn.style.display = "block";
 });
 
+const projectDetails = [
+  {
+    id: 1,
+    name: "Roland change",
+    description:
+      "Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list.",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+  {
+    id: 2,
+    name: "amani-fest",
+    description:
+      "Amani-fest is a project to design a platform for Amani Festival which is the annual festival where people get united to sing, dance, and proclaim peace. Take place in February every year in Goma Town/DRCongo. With this platform you will get all information about the Festival, and even book the ticket for the event.",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+  {
+    id: 3,
+    name: "Math Magician",
+    description:
+      "Math magicians is a website for all fans of mathematics, where people can Make simple calculations and read a random math-related quote.",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+  {
+    id: 4,
+    name: "The Meal",
+    description:
+      "This is a web application that allows you to get information about various kinds of meals and recipes from all over the world. Using, MealzDB you can get information about various meals and recipes from all over the world",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+  {
+    id: 5,
+    name: "mi_bugdet ",
+    description:
+      "It's a apps to help you out to manage your bugdet whereby you ca n Add a category and transactions that belongs to that category. ",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+  {
+    id: 6,
+    name: "Bob's Gurger Anime ",
+    description:
+      "It's a mobile web application to display the list of characters for the Bob's Gurger Anime show. Fetcher from the Bob's Burger API",
+    featuredImageUrl: "pop-up-img2.png",
+    technologies: ["HTM", "CSS", "Javascript"],
+    liveVersion: "#",
+    sourceURL: "#",
+  },
+];
+
+const popUpCallback = (id) => {
+  const project = projectDetails.find((project) => project.id === +id);
+
+  return `
+  <div id="popblur"></div>
+  <div class="pop-up" id="popup">
+  <div class="pop-container">
+    <div class="pop-up2">
+      <button class="button-pop" type="button"  onclick="closePopup()"><i class="fa-solid fa-xmark"></i></button>
+    </div>
+   
+    <img class="pop-img" src="${project.featuredImageUrl}"> 
+    <h2 class="pop-h2">${project.name}</h2>
+
+      <div class="pop-fin">
+        
+        <a href="${
+          project.liveVersion
+        }" class="POP-BTN">See live <i class="fa-light fa-timer"></i></a>
+        <a href="${
+          project.sourceURL
+        }" class="POP-BTN">See source <i class="fa-brands fa-github"></i></a>
+      </div>
+      
+    <ul class="pop-skills">
+      ${project.technologies
+        .map((tech) => `<li class="skills-up">${tech}</li>`)
+        .join("")}
+    </ul>
+   <p class="par-pop">${project.description} </p>
+
+  </div>
+
+</div>  
+  `;
+};
+
 closeBtn.addEventListener("click", function (e) {
   e.preventDefault();
   mobileMenu.style.display = "none";
@@ -28,108 +128,14 @@ navLinks.forEach((link) => {
   });
 });
 
-const projectDetails = [
-  {
-    id: 1,
-    name: "Roland change",
-    description:
-      "Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list.",
-    featuredImageUrl: "images/awesome-books.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://bateyjosue.github.io/awesome-books/",
-    sourceURL: "https://github.com/Bateyjosue/awesome-books",
-  },
-  {
-    id: 2,
-    name: "amani-fest",
-    description:
-      "Amani-fest is a project to design a platform for Amani Festival which is the annual festival where people get united to sing, dance, and proclaim peace. Take place in February every year in Goma Town/DRCongo. With this platform you will get all information about the Festival, and even book the ticket for the event.",
-    featuredImageUrl: "./images/amani.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://amani-fest-jb.netlify.app/",
-    sourceURL: "https://github.com/Bateyjosue/amani-fest",
-  },
-  {
-    id: 3,
-    name: "Math Magician",
-    description:
-      "Math magicians is a website for all fans of mathematics, where people can Make simple calculations and read a random math-related quote.",
-    featuredImageUrl: "./images/math.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://math-magician-jb.netlify.app/",
-    sourceURL: "https://github.com/Bateyjosue/math_magician",
-  },
-  {
-    id: 4,
-    name: "The Meal",
-    description:
-      "This is a web application that allows you to get information about various kinds of meals and recipes from all over the world. Using, MealzDB you can get information about various meals and recipes from all over the world",
-    featuredImageUrl: "./images/meals.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://bateyjosue.github.io/The-Meal/",
-    sourceURL: "https://github.com/Bateyjosue/The-Meal",
-  },
-  {
-    id: 5,
-    name: "mi_bugdet ",
-    description:
-      "It's a apps to help you out to manage your bugdet whereby you ca n Add a category and transactions that belongs to that category. ",
-    featuredImageUrl: "./images/budget.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://www.loom.com/share/7b1e4eb40a95466a81104a080ffe2042",
-    sourceURL: "https://github.com/Bateyjosue/mi_bugdet",
-  },
-  {
-    id: 6,
-    name: "Bob's Gurger Anime ",
-    description:
-      "It's a mobile web application to display the list of characters for the Bob's Gurger Anime show. Fetcher from the Bob's Burger API",
-    featuredImageUrl: "./images/bob.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "https://bob-burger-jb.netlify.app/",
-    sourceURL: "https://github.com/Bateyjosue/metrics-webapp",
-  },
-];
-
-const popblur = document.getElementById("popblur");
-function openPopup() {
-  popup.classList.add("open-pop-up");
-  popblur.style.display = "block";
+const cardPopUpContainer = document.querySelector(".popUpDynamic");
+function openPopup(id) {
+  const popUpHTMLStr = popUpCallback(id);
+  cardPopUpContainer.insertAdjacentHTML("beforeend", popUpHTMLStr);
 }
 
-const modalPopup = (index) => {
-  modalDetails.innerHTML = `
-    <div class="modal-image">
-      <span class="modal-close-btn"><i class="bi bi-x-lg"></i></span>
-      <img src="${projectDetails[index].featuredImageUrl}" alt="" width="300" height="180">
-    </div>
-    <div class="modal-body card-content">
-      <h2 class="title">${projectDetails[index].name}</h2>
-      <ul>
-        ${projectDetails[index].technologies.map((lang) => `<li>${lang} </li>`).join('')}
-      </ul>
-      <p class="more">
-      ${projectDetails[index].description}
-      </p>
-    </div>
-    <div class="modal-button">
-      <a href="${projectDetails[index].liveVersion}" class="button">See Live <i class="bi bi-arrow-up-right-circle"></i></a>
-      <a href="${projectDetails[index].sourceURL}" class="button">See Source <i class="bi bi-github"></i></a>
-    </div>
-  `;
-
-  const modalCloseBtn = document.querySelectorAll('#works .modal .modal-image .modal-close-btn');
-  modalCloseBtn.forEach((el) => {
-    el.addEventListener('click', () => {
-      modalDetails.style.visibility = 'hidden';
-      blurryGray.style.visibility = 'hidden';
-    });
-  });
-};
-
 function closePopup() {
-  popup.classList.remove("open-pop-up");
-  popblur.style.display = "none";
+  cardPopUpContainer.textContent = "";
 }
 
 const userEmail = document.getElementById("userEmail");
@@ -179,6 +185,12 @@ window.onload = () => {
 
 const cardContainer = document.querySelector("#cards");
 
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-skill")) {
+    openPopup(e.target.id);
+  }
+});
+
 projectDetails.forEach((el) => {
   cardContainer.innerHTML += `
   <div class="card">
@@ -190,10 +202,9 @@ projectDetails.forEach((el) => {
         .map((lang) => `<li class="skill">${lang} </li>`)
         .join("")}
     </ul>
-    <button class="btn btn-skill" onclick="openPopup()" value="${
-      el.id
-    }">See Project</button>
+    <button class="btn btn-skill" id="${el.id}">See Project</button> 
   </div>
 </div>
   `;
 });
+
