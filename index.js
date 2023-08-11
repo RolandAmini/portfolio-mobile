1268a89550b53a7640e66a344430b02c6e2283a8
FilbertKimasa <filbertdan67@gmail.com>
2023-08-11 14:56:18 +0300

    Modify index.js file

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const closeBtn = document.getElementById("close-btn");
-const mobileMenu = document.querySelector(".nav-menu");
-const emailInput = document.getElementById("userEmail");
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const closeBtn = document.getElementById('close-btn');
+const mobileMenu = document.querySelector('.nav-menu');
+const emailInput = document.getElementById('userEmail');
 
-hamburgerBtn.addEventListener("CLICK", function (e) {
+hamburgerBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  mobileMenu.style.display = "block";
-  hamburgerBtn.style.display = "none";
-  closeBtn.style.display = "block";
+  mobileMenu.style.display = 'block';
+  hamburgerBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
 });
 
 const projectDetails = [
   {
     id: 1,
-    name: "Roland change",
+    name: 'Roland change',
     description:
-      "Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list.",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+      'Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list.',
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
   {
     id: 2,
-    name: "amani-fest",
+    name: 'amani-fest',
     description:
-      "Amani-fest is a project to design a platform for Amani Festival which is the annual festival where people get united to sing, dance, and proclaim peace. Take place in February every year in Goma Town/DRCongo. With this platform you will get all information about the Festival, and even book the ticket for the event.",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+      'Amani-fest is a project to design a platform for Amani Festival which is the annual festival where people get united to sing, dance, and proclaim peace. Take place in February every year in Goma Town/DRCongo. With this platform you will get all information about the Festival, and even book the ticket for the event.',
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
   {
     id: 3,
-    name: "Math Magician",
+    name: 'Math Magician',
     description:
-      "Math magicians is a website for all fans of mathematics, where people can Make simple calculations and read a random math-related quote.",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+      'Math magicians is a website for all fans of mathematics, where people can Make simple calculations and read a random math-related quote.',
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
   {
     id: 4,
-    name: "The Meal",
+    name: 'The Meal',
     description:
-      "This is a web application that allows you to get information about various kinds of meals and recipes from all over the world. Using, MealzDB you can get information about various meals and recipes from all over the world",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+      'This is a web application that allows you to get information about various kinds of meals and recipes from all over the world. Using, MealzDB you can get information about various meals and recipes from all over the world',
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
   {
     id: 5,
-    name: "mi_bugdet ",
+    name: 'mi_bugdet ',
     description:
       "It's a apps to help you out to manage your bugdet whereby you ca n Add a category and transactions that belongs to that category. ",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
   {
     id: 6,
     name: "Bob's Gurger Anime ",
     description:
       "It's a mobile web application to display the list of characters for the Bob's Gurger Anime show. Fetcher from the Bob's Burger API",
-    featuredImageUrl: "pop-up-img2.png",
-    technologies: ["HTM", "CSS", "Javascript"],
-    liveVersion: "#",
-    sourceURL: "#",
+    featuredImageUrl: 'pop-up-img2.png',
+    technologies: ['HTM', 'CSS', 'Javascript'],
+    liveVersion: '#',
+    sourceURL: '#',
   },
 ];
 
@@ -100,7 +100,7 @@ const popUpCallback = (id) => {
     <ul class="pop-skills">
       ${project.technologies
         .map((tech) => `<li class="skills-up">${tech}</li>`)
-        .join("")}
+        .join('')}
     </ul>
    <p class="par-pop">${project.description} </p>
 
@@ -110,51 +110,51 @@ const popUpCallback = (id) => {
   `;
 };
 
-closeBtn.addEventListener("click", function (e) {
+closeBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  mobileMenu.style.display = "none";
-  hamburgerBtn.style.display = "block";
-  closeBtn.style.display = "none";
+  mobileMenu.style.display = 'none';
+  hamburgerBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
 });
 
-const navLinks = document.querySelectorAll("nav ul li a");
-const navMenu = document.querySelector("nav ul");
+const navLinks = document.querySelectorAll('nav ul li a');
+const navMenu = document.querySelector('nav ul');
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    mobileMenu.style.display = "none";
-    hamburgerBtn.style.display = "block";
-    closeBtn.style.display = "none";
+  link.addEventListener('click', (e) => {
+    mobileMenu.style.display = 'none';
+    hamburgerBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
   });
 });
 
-const cardPopUpContainer = document.querySelector(".popUpDynamic");
+const cardPopUpContainer = document.querySelector('.popUpDynamic');
 function openPopup(id) {
   const popUpHTMLStr = popUpCallback(id);
-  cardPopUpContainer.insertAdjacentHTML("beforeend", popUpHTMLStr);
+  cardPopUpContainer.insertAdjacentHTML('beforeend', popUpHTMLStr);
 }
 
 function closePopup() {
-  cardPopUpContainer.textContent = "";
+  cardPopUpContainer.textContent = '';
 }
 
-const userEmail = document.getElementById("userEmail");
+const userEmail = document.getElementById('userEmail');
 
-const myForm = document.getElementById("myForm");
+const myForm = document.getElementById('myForm');
 
-myForm.addEventListener("submit", function (e) {
+myForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const errorMessage = document.getElementById("error-message");
-  const fullname = document.querySelector("#fullname");
+  const errorMessage = document.getElementById('error-message');
+  const fullname = document.querySelector('#fullname');
 
   const email = emailInput.value.trim();
 
   // validate email to not be in lowercase1
   if (email === email.toLowerCase()) {
-    errorMessage.textContent = "";
+    errorMessage.textContent = '';
   } else {
-    errorMessage.textContent = "Please use lowercase letters";
+    errorMessage.textContent = 'Please use lowercase letters';
   }
 
   // local storage
@@ -163,30 +163,30 @@ myForm.addEventListener("submit", function (e) {
     name: fullname.value,
   };
 
-  localStorage.setItem("formData", JSON.stringify(formData));
+  localStorage.setItem('formData', JSON.stringify(formData));
 
-  document.getElementById("userEmail").value = "";
-  document.getElementById("message").value = "";
-  document.querySelector("#fullname").value = "";
+  document.getElementById('userEmail').value = '';
+  document.getElementById('message').value = '';
+  document.querySelector('#fullname').value = '';
 });
 
 // Receive data from local-storage
 window.onload = () => {
-  const formData = localStorage.getItem("formData");
+  const formData = localStorage.getItem('formData');
   if (formData) {
     // Check if the data exists
     const formDataObject = JSON.parse(formData);
-    if (fullname) fullname.value = formDataObject.name || "";
-    if (emailInput) emailInput.value = formDataObject.email || "";
+    if (fullname) fullname.value = formDataObject.name || '';
+    if (emailInput) emailInput.value = formDataObject.email || '';
   }
 };
 
 // pop up and cards
 
-const cardContainer = document.querySelector("#cards");
+const cardContainer = document.querySelector('#cards');
 
-cardContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn-skill")) {
+cardContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('btn-skill')) {
     openPopup(e.target.id);
   }
 });
@@ -200,11 +200,10 @@ projectDetails.forEach((el) => {
     <ul class="skills">
       ${el.technologies
         .map((lang) => `<li class="skill">${lang} </li>`)
-        .join("")}
+        .join('')}
     </ul>
     <button class="btn btn-skill" id="${el.id}">See Project</button> 
   </div>
 </div>
   `;
 });
-
